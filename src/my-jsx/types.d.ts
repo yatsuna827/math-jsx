9106fcb720d279d3e8762fe6b712d352f1784c9f
yatsuna827 <yatsuna_827@yahoo.co.jp>
2025-03-22 01:03:24 +0900

    feat: 独自JSXに型を定義

---
+++ src/my-jsx/types.d.ts
@@ -0,0 +1,13 @@
+interface HasChildren {
+  children?: string | string[];
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    div: HasChildren & { key?: string };
+    h1: HasChildren & { id?: string };
+    p: HasChildren;
+  }
+
+  type Element = string;
+}
